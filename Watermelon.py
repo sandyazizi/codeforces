73bdb72dfe1adccbdbb20caaf8a017b36d59bf6c
senzen <121148549+senzen@users.noreply.github.com>
2022-12-21 14:26:11 +0700

    Add files via upload

---
+++ Watermelon.py
@@ -0,0 +1,11 @@
+import math
+
+weight = int(input(""))
+
+if(weight > 2):
+    if(weight % 2 == 0):
+        print("Yes")
+    else:
+        print("No")
+else:
+    print("No")
---
+++ way_too_long_word.py
@@ -0,0 +1,10 @@
+
+list = int(input(""))
+
+for(i = 0; i < list):
+    nama = input("")
+    if(len(nama)):
+        print(nama[0] ;len(nama);nama[len(nama)-1])
+    else{
+        print(nama)
+    }
